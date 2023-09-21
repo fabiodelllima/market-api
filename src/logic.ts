78d3a2fa7616f86e485d7c111536144dc4705cf5
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-21 01:18:42 -0300

    feat: Lógica para atualização de produtos

--- src/logic.ts
+++ src/logic.ts
@@ -25,3 +25,24 @@ export const readOneProduct = (req: Request, res: Response) => {
 
   return res.status(200).json(product);
 };
+
+export const updateProduct = (req: Request, res: Response) => {
+  const index = market.findIndex((product) => {
+    return product.id === Number(req.params.productId);
+  });
+
+  const newProduct = {
+    id: Number(req.params.productId),
+    name: String(req.body.name),
+    price: Number(req.params.price),
+    weight: Number(req.params.weight),
+    section: String(req.params.section),
+    calories: Number(req.params.calories),
+    // expirationDate: Date(),
+  };
+
+  market.splice(index, 1, newProduct);
+
+  return res.status(200).json();
+};
+
