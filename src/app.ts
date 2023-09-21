49550c752be4145e1e3634bfad0074d025554bb9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-21 01:02:22 -0300

    chore: Tratamento de rota para produtos

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from 'express';
+import { productRouter } from './routes/products.routes';
+
 const app: Application = express();
 const PORT: number = 3000;
 const runningMsg: string = `Server running on http://localhost:${PORT}`;
 
 app.use(express.json());
+app.use('/products', productRouter);
