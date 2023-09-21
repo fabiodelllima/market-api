6b3caadb6421db8d4c89f6b208fccd7a9e0a488b
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-21 01:09:06 -0300

    feat: Definição de interface para produto

---
+++ src/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  weight: number;
+  section: string;
+  calories: number | null | undefined;
+  // expirationDate: Date;
+}
