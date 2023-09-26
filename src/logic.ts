21e331f96ec4d8a59d20a72c6698331d136b96af
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-25 23:17:03 -0300

    refactor: Lógica reformulada na geração de ID

--- src/logic.ts
+++ src/logic.ts
@@ -3,10 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { market } from './database';
 import { IProduct } from './interfaces';
 
+let id = 1;
+
 export const createProduct = (req: Request, res: Response) => {
   const newProduct: IProduct = { id: uuidv4(), ...req.body };
 
   market.push(newProduct);
+  id++;
 
   return res.status(201).json({
     message: 'Product succesfully created',
